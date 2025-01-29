e57d2e14e28e9583632852c7fff754139fe3c90d
Artie <artierosser@gmail.com>
2025-01-28 21:18:16 -0600

    created accounts

---
+++ db/migrate/20250129031529_create_accounts.rb
@@ -0,0 +1,8 @@
+class CreateAccounts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :accounts do |t|
+      t.timestamps
+      t.string :uuid, default: 'gen_random_uuid()', null: false
+    end
+  end
+end
