9017171246cd4da500ec528d800fb532335a4dde
Artie <artierosser@gmail.com>
2025-02-06 00:08:07 +0000

    added happy path

--- spec/policies/course_policy_spec.rb
+++ spec/policies/course_policy_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe CoursePolicy, type: :policy do
         expect(Pundit.policy(user, Course).new?).to be(false)
       end
     end
+
+    context "when the user has an instructor profile" do
+      it "should be true" do
+        user.profile.build_instructor.save
+        expect(Pundit.policy(user, Course).new?).to be(true)
+      end
+    end
   end
 end
